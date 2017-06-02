da08ec0426e17f1c8f30ecbb3855792858b2644b
Troy <troy.halpin@procore.com>
2017-06-02 14:10:13 -0700

    added logging out function and testing, finished chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
 
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
